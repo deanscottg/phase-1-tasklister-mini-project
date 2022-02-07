821d187c0bddcc0117b3c601b0c99aea72721578
Dean <deanscottg@gmail.com>
2022-02-07 11:22:30 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ document.querySelector('form').addEventListener('submit',function (e) {
+     e.preventDefault()
+       logTask((e.target["new-task-description"].value)
+       )
+
+   });
+//  {console.log(task)
+
+function logTask (task) {
+const p = document.createElement('p')
+p.textContent = task
+// console.log(p)('tasks')
+document.querySelector('#tasks').appendChild(p)
+}
+})
